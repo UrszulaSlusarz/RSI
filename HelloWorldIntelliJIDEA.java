cd3f2e5b04e5438e029049733fb1ee7723495359
UrszulaSlusarz <32739935+UrszulaSlusarz@users.noreply.github.com>
2018-03-07 09:23:20 +0100

    Add files via upload

---
+++ HelloWorldIntelliJIDEA.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class HelloWorldIntelliJIDEA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Urszula Slusarz-IO_RSI");
+    }
+}
